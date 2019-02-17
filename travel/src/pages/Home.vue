31f3dc2e05c76898284bd4f3c445843123fb8932
hcwen <31315495+hcwen@users.noreply.github.com>
2019-02-17 16:22:39 +0800

    add header

---
+++ travel/src/pages/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+     <homeheader></homeheader>
+  </div>
+</template>
+
+<script>
+import HomeHeader from './components/Header'
+export default {
+  name: 'Home',
+  data () {
+    return {
+
+    }
+    components:{
+      HomeHeader:HomeHeader
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
